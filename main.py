e281a01a4bc0a8d21c1e1c2abbf2098c33f18bad
jsms4825 <jsms4825@naver.com>
2023-10-18 12:00:55 +0000

    main finished

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from typing import List
 
+def even_list(int_list: List[int]) -> List[int]:
+
+    
+    
+    
+    
+    
+    
+    # TODO: Implement even_list
+    pass
+# Skeleton code for sum_of_squares_of_even
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+
+    
+    
+    
+    
+    
+    
+    # TODO: Implement sum_of_squares_of_even
+    pass
+
 # Main function
 def main():
     # Example list
