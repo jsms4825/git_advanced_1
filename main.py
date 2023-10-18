2710de13aaff8da20ca06bb8990e793b6029bdc2
jsms4825 <jsms4825@naver.com>
2023-10-18 11:39:22 +0000

    even_list finished

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from typing import List
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
 
+    evenList = []
+    for i in int_list:
+        if i%2 == 0:
+            evenList.append(i)
+    
+    return evenList
+
     # TODO: Implement even_list
     pass
 
